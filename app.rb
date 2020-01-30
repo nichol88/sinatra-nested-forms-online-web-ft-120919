9ba42a6f95ffff10466dbf6e347d06d3e8e736d8
Brian Nicholls <nichol88@gmail.com>
2020-01-30 08:55:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params['pirate']['name'], pirate['height'], pirate['weight'] )
+      @pirate = Pirate.new(params['pirate']['name'], params['pirate']['height'], params['pirate']['weight'] )
 
       params['pirate']['ships'].each do |ship_params|
         Ship.new(ship_params['name'], ship_params['type'], ship_params['booty'])
